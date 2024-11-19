bf66553a88c604ba1d9ef20d1a9f8427a75675f6
janjaw007 <chayanit.kitm@gmail.com>
2024-11-19 23:04:29 +0700

    add log for query using GORM Logger

--- main.go
+++ main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"time"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 const (
@@ -22,7 +26,19 @@ func main() {
 		"password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
 
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	// New logger for detailed SQL logging
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold: time.Second, // Slow SQL threshold time that start query
+			LogLevel:      logger.Info, // Log level
+			Colorful:      true,        // Enable color
+		},
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: newLogger, // add Logger
+	})
 
 	if err != nil {
 		// panic = kill process
@@ -35,4 +51,5 @@ func main() {
 	// AutoMigrate will compare struct and table on sql
 	// AutoMigrate Recive 2 argument (struct, ) that strcut is structure for tables will be create
 	db.AutoMigrate(&Book{})
+	fmt.Println("Migrate successful!")
 }
