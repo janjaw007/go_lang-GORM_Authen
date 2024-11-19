7f12b6e8c5a231c4f307696b2399ebe98d0d1017
janjaw007 <chayanit.kitm@gmail.com>
2024-11-19 22:48:47 +0700

    create connection to db

--- main.go
+++ main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+const (
+	host     = "localhost"  // or the Docker service name if running in another container
+	port     = 5432         // default PostgreSQL port
+	user     = "myuser"     // as defined in docker-compose.yml
+	password = "mypassword" // as defined in docker-compose.yml
+	dbname   = "mydatabase" // as defined in docker-compose.yml
+)
+
+func main() {
+	// Create Connection String
+	// Configure your PostgreSQL database details here
+	dsn := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		// panic = kill process
+		panic("failed to connect database")
+	}
+
+	fmt.Println(db)
+	fmt.Println("Connect Successful!")
 }
